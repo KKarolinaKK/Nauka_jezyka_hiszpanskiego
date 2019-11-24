7704a4c3e810b9b5f60a1d7703afe976be85ea57
KKarolinaKK <50325349+KKarolinaKK@users.noreply.github.com>
2019-11-24 11:57:37 +0100

    notatki

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
-from scipy import stats
+from django.shortcuts import redirect
 
 import csv
 import pandas as pd
 
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'notatka.html', {'form': form})
 
 def home(request):
     print('Strona główna')
